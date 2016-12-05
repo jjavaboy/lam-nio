8c86103b5479bcb59b0b8686b7fd444a7055d45a
jjavaboy <jjavaboy@163.com>
2016-12-05 15:49:09 +0800

    添加分片的java客户端MongoClient注释说明

--- lam-mongodb/src/main/java/lam/mongo/db/ShardMongoDBClient.java
+++ lam-mongodb/src/main/java/lam/mongo/db/ShardMongoDBClient.java
@@ -32,6 +32,16 @@ public class ShardMongoDBClient {
 		seeds.add(seed1);
 		seeds.add(seed2);
 		seeds.add(seed3);
+		
+//MongoClient注释说明，只会使用其中的一台mongos连接。
+/**
+* Creates a Mongo based on a list of replica set members or a list of mongos. It will find all members (the master will be used by
+*  default). If you pass in a single server in the list, the driver will still function as if it is a replica set. 
+*  If you have a standalone server, use the Mongo(ServerAddress) constructor.
+
+* If this is a list of mongos servers, it will pick the closest (lowest ping time) one to send all requests to, and automatically
+*  fail over to the next server if the closest is down.
+*/
 		mongoClient = new MongoClient(seeds);
 		System.out.println("Shard MongoClient init:" + seeds);
 	}
