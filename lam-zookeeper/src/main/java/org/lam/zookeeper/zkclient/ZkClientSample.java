78b6effb1c2f846d14607537ce9df3038c1bd60b
jjavaboy <jjavaboy@163.com>
2017-03-05 23:39:02 +0800

    zookeeper zkclient auth

--- lam-zookeeper/src/main/java/org/lam/zookeeper/zkclient/ZkClientSample.java
+++ lam-zookeeper/src/main/java/org/lam/zookeeper/zkclient/ZkClientSample.java
@@ -1,6 +1,8 @@
 package org.lam.zookeeper.zkclient;
 
 import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -12,9 +14,15 @@ import org.I0Itec.zkclient.exception.ZkMarshallingError;
 import org.I0Itec.zkclient.serialize.SerializableSerializer;
 import org.I0Itec.zkclient.serialize.ZkSerializer;
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.data.ACL;
+import org.apache.zookeeper.data.Id;
 import org.apache.zookeeper.data.Stat;
+import org.apache.zookeeper.server.auth.DigestAuthenticationProvider;
 
 import com.google.gson.Gson;
+
+import lam.log.Console;
 
 /**
 * <p>
@@ -28,11 +36,11 @@ public class ZkClientSample {
 	
 	static Gson gson = new Gson();
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		int connectionTimeOut = Integer.MAX_VALUE;//time out for connecting to server
 		ZkClient zkClient = new ZkClient(buildDefaultZkConnection(), connectionTimeOut, buildDefaultZkSerializer());
 	
-		String path = "/test";
+		String path = "/test1";
 		String rootPath = "/";
 		
 		//subscribe for node children list change
@@ -81,11 +89,48 @@ public class ZkClientSample {
 		zkClient.delete(path);
 		
 		//权限控制=====================================
+		String digest = "digest"; //schema
+		
+		String superAuth   = "superUser:111111";
+		String createAuth  = "createUser:111111";      //CREATE(c): 创建权限，可以创建当前node的子节点
+		String deleteAuth  = "deleteUser:111111";	   //DELETE(d): 删除权限，可以删除当前node的子节点
+		String readAuth    = "readUser:111111";   	   //READ(r): 读权限，可以获取当前node的数据，可以list当前node所有的child nodes
+		String writeAuth   = "writeUser:111111";  	   //WRITE(w): 写权限，可以向当前node写数据
+		String adminAuth   = "adminUser:111111";       //ADMIN(a): 管理权限，可以设置当前node的permission
 		
 		//schema:权限控制模式(world,auth,digest,ip)
 		//auth:foo:true，类似:username:password
-		zkClient.addAuthInfo("digest", "foo:true".getBytes());
+		//先添加一个认证用户
+		zkClient.addAuthInfo(digest, superAuth.getBytes());
 		
+		List<ACL> acls = new ArrayList<ACL>();
+		
+		acls.add(new ACL(ZooDefs.Perms.ALL, new Id(digest, DigestAuthenticationProvider.generateDigest(superAuth))));
+		acls.add(new ACL(ZooDefs.Perms.CREATE, new Id(digest, DigestAuthenticationProvider.generateDigest(createAuth))));
+		acls.add(new ACL(ZooDefs.Perms.DELETE, new Id(digest, DigestAuthenticationProvider.generateDigest(deleteAuth))));
+		acls.add(new ACL(ZooDefs.Perms.READ, new Id(digest, DigestAuthenticationProvider.generateDigest(readAuth))));
+		acls.add(new ACL(ZooDefs.Perms.WRITE, new Id(digest, DigestAuthenticationProvider.generateDigest(writeAuth))));
+		acls.add(new ACL(ZooDefs.Perms.ADMIN, new Id(digest, DigestAuthenticationProvider.generateDigest(adminAuth))));
+		
+		path = "/test";
+		//zkClient.createPersistent(path, data, acls);
+		/**
+		[zk: 192.168.20.111:2181(CONNECTED) 8] getAcl /test
+		'digest,'superUser:vLmzknrXH9dGsV4jNWsPmK2gqUU=
+		: cdrwa
+		'digest,'createUser:vqAQivdWW2drkS8FT+LjmAbUY4k=
+		: c
+		'digest,'deleteUser:mHEOW8g3eARHH2FcfvbTIB7uAYo=
+		: d
+		'digest,'readUser:EVdGv4F9QkpvcZYCa99HF110xGQ=
+		: r
+		'digest,'writeUser:E1L5Eqbt3mwODQm4Rhaa+Qzyxb8=
+		: w
+		'digest,'adminUser:iioWqkg9vM4tfkUhuAgLJt9Wjgs=
+		: a		 
+		 */
+		
+		Console.println("AuthNode data:" + gson.toJson(zkClient.readData(path)));
 		
 
 		waitSelf();
