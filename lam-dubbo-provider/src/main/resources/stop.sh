b47d2444a3de122042fcbfcbfe04380939de94ea
jjavaboy <jjavaboy@163.com>
2017-05-05 13:15:48 +0800

    stop app shell

--- lam-dubbo-provider/src/main/resources/stop.sh
+++ lam-dubbo-provider/src/main/resources/stop.sh
@@ -2,17 +2,15 @@
 
 APP_MAIN="lam.dubbo.provider.start.LamDubboProviderStartUp"
 
-APP_ID=0
+APP_PID=0
 
 # get pid of this app, which main class is ${APP_MAIN}
-#
 getAppPid(){
     COMMAND_RS=`${JAVA_HOME}/bin/jps -l | grep ${APP_MAIN}`
-    if [ -n "${COMMAND_RS}" ];
-    then
+    if [ -n "${COMMAND_RS}" ]; then
         APP_PID=`echo ${COMMAND_RS} | awk '{print $1}'`
     else
-	APP_PID=0
+        APP_PID=0
     fi
 }
 
@@ -22,15 +20,15 @@ shutdown(){
         echo "Stopping ${APP_MAIN}..."
         kill ${APP_PID}
         getAppPid
-        if [ ${APP_PID} -ne 0 ]; then
-	    echo "Fail to stop it."
+        if [ ${APP_PID} -eq 0 ]; then
+            echo "Stop ${APP_MAIN} success."
         else
-            echo "Stop success."
+            echo "Fail to stop ${APP_MAIN}."
         fi
     else
-	echo "${APP_MAIN} is not running."
+        echo "${APP_MAIN} is not running."
     fi
 }
 
-# call stop function
+# call function of stop
 shutdown
