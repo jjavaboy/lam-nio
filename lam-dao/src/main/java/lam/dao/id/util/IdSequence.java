532ec0ef2e93c7cf69251b2133d770e6dd681995
jjavaboy <jjavaboy@163.com>
2017-08-10 18:41:18 +0800

    create id in distribution enviroment

---
+++ lam-dao/src/main/java/lam/dao/id/util/IdSequence.java
@@ -0,0 +1,81 @@
+package lam.dao.id.util;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import lam.dao.id.model.Sequence;
+import lam.dao.id.service.SequenceService;
+
+/**
+* <p>
+* TODO
+* </p>
+* @author linanmiao
+* @date 2017年8月10日
+* @version 1.0
+*/
+public class IdSequence {
+	
+	private static volatile IdSequence INSTANCE;
+	
+	private Logger logger = LoggerFactory.getLogger(IdSequence.class);
+	
+	private int cacheSize;
+	
+	private SequenceService sequenceService;
+	
+	private ConcurrentHashMap<String, ConcurrentLinkedQueue<Long>> sequenceQueue;
+	
+	private IdSequence(){
+		cacheSize = 50;
+		sequenceService = SpringContextHolder.getBean(SequenceService.class);
+		sequenceQueue = new ConcurrentHashMap<String, ConcurrentLinkedQueue<Long>>();
+	}
+	
+	public static IdSequence getInstance(){
+		if(INSTANCE == null){
+			synchronized (IdSequence.class) {
+				if(INSTANCE == null){
+					INSTANCE = new IdSequence();
+				}
+			}
+		}
+		return INSTANCE;
+	}
+	
+	public /**synchronized*/ Long nextId(String sequenceName){
+		ConcurrentLinkedQueue<Long> idQueue = sequenceQueue.get(sequenceName);
+		if(idQueue == null){
+			//putIfAbsent method ensure synchronized
+			sequenceQueue.putIfAbsent(sequenceName, new ConcurrentLinkedQueue<Long>());
+			idQueue = sequenceQueue.get(sequenceName);
+		}
+		//double check id queue size
+		if(idQueue.size() < cacheSize){
+			synchronized(idQueue){
+				if(idQueue.size() < cacheSize){
+					Sequence sequence = sequenceService.getSequence(sequenceName);
+					if(sequence == null)
+						throw new NullPointerException("Can't get Sequence from name " + sequenceName);
+					long id = sequence.getOldValue();
+					while(id < sequence.getNewValue()){
+						idQueue.offer(Long.valueOf(id++));
+					}
+				}
+			}
+		}
+		Long idValue = idQueue.poll();
+		if(idValue == null){
+			//throw new NullPointerException("Can't get id value from Sequeue named " + sequenceName);
+			logger.warn("Can't get id value from Sequeue named " + sequenceName);
+			//spin to get next id
+			idValue = nextId(sequenceName);
+		}
+		
+		return idValue;
+	}
+
+}
