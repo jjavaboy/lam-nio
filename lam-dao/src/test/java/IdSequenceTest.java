a115e56eef607469c3e473757621a9484ed5915e
jjavaboy <jjavaboy@163.com>
2017-08-11 17:39:00 +0800

    IdSequenceTest

--- lam-dao/src/test/java/IdSequenceTest.java
+++ lam-dao/src/test/java/IdSequenceTest.java
@@ -1,6 +1,13 @@
+import java.io.Serializable;
+import java.util.concurrent.CompletionService;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -24,14 +31,20 @@ public class IdSequenceTest {
 	
 	private static ClassPathXmlApplicationContext context;
 	
+	private static ThreadPoolExecutor executor;
+	
 	private static Logger logger = LoggerFactory.getLogger(IdSequenceTest.class);
 	
 	static{
 		Runtime.getRuntime().addShutdownHook(new Thread(){
 			@Override
 			public void run() {
+				logger.info("start to clear resource");
 				if(context != null)
 					context.close();
+				if(executor != null)
+					executor.shutdown();
+				logger.info("end to clear resource");
 			}
 		});
 	}
@@ -39,7 +52,7 @@ public class IdSequenceTest {
 	public static void main(String[] args){
 		context = new ClassPathXmlApplicationContext(new String[]{"applicationContext.xml"});
 		context.start();
-		ThreadPoolExecutor executor = new ThreadPoolExecutor(8, 8, 0, TimeUnit.MICROSECONDS, 
+		executor = new ThreadPoolExecutor(20, 20, 0, TimeUnit.MICROSECONDS, 
 				new LinkedBlockingQueue<Runnable>(),
 				new ThreadFactoryBuilder().setThreadNamePrefix("IdSequenceTest").build(),
 				new RejectedExecutionHandler() {
@@ -58,9 +71,13 @@ public class IdSequenceTest {
 		
 		final ConcurrentHashMap<Long, Boolean> idMap = new ConcurrentHashMap<Long, Boolean>();
 		
+		/*CompletionService*/ExecutorCompletionService<String> completionService = new ExecutorCompletionService<String>(executor);
+		LinkedBlockingQueue<Future<String>> resultQueue = new LinkedBlockingQueue<Future<String>>();
+		final CountDownLatch latch = new CountDownLatch(100000);
 		long start = System.currentTimeMillis();
 		for(int i = 0; i < 100000; i++){
-			executor.execute(new Runnable(){
+			resultQueue.offer(
+			/*completionService*/executor.submit(new Runnable(){
 				@Override
 				public void run() {
 					long id = IdSequence.getInstance().nextId(Foo.class.getSimpleName());
@@ -68,13 +85,36 @@ public class IdSequenceTest {
 						logger.info(Thread.currentThread().getName() + "-" + "duplicate id:" + id);
 					}else{
 						idMap.put(id, false);
-						logger.info(Thread.currentThread().getName() + "-" + "unique id:" + id);
+						//logger.info(Thread.currentThread().getName() + "-" + "unique id:" + id);
 					}
+					latch.countDown();
 				}
-			});
+			}, "task " + i + " callback")
+			);
 		}
-		long end = System.currentTimeMillis();
-		//logger.info("cost(ms):" + (end - start));
+		
+		//to check whether task has been done
+		try {
+			/*for(int j = 0; j < 100000; j++){
+				//completionService.take();
+				resultQueue.take().get();
+			}*/
+			latch.await();
+			long end = System.currentTimeMillis();
+			logger.info("cost(ms):" + (end - start));
+		} catch (Exception e) {
+			logger.error("error", e);
+		}
+		
+		logger.info("Demo id:" + IdSequence.getInstance().nextId(Demo.class.getSimpleName()));
+		
+		System.exit(0);
+	}
+	
+	private static class Demo implements Serializable{
+
+		private static final long serialVersionUID = -6486479385581032575L;
+		
 	}
 
 }
