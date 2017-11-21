2aebd2c10141f3c61ba23ed09fc5571075eaae06
jjavaboy <jjavaboy@163.com>
2017-11-21 17:50:46 +0800

    CLH lock

--- lam-nio-core/src/main/java/lam/concurrent/lock/CLHLock.java
+++ lam-nio-core/src/main/java/lam/concurrent/lock/CLHLock.java
@@ -4,6 +4,7 @@ import java.util.Random;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 import lam.concurrent.lock.support.LLock;
@@ -52,41 +53,52 @@ public class CLHLock implements LLock{
 		qnode.locked = Boolean.TRUE.booleanValue(); //当前线程需要获得锁
 		QNode pred = tail.getAndSet(qnode); //取到前趋节点QNode pred，然后设置当前线程的节点到队尾
 		myPred.set(pred); //设置当前线程的前趋节点QNode pred
+		Console.println("before myNode:%s, myPred:%s", qnode, pred);
 		while (pred.locked){} //一直循环直到前趋节点pred释放锁（pred.locked为false）
+		Console.println("after myNode:%s, myPred:%s", qnode, pred);
 	}
 
 	@Override
 	public void unlock() {
 		QNode qnode = myNode.get(); //当前线程锁的QNode节点
 		qnode.locked = Boolean.FALSE.booleanValue(); //设置当前线程释放锁的状态
+		Console.println("myNode:%s, myPred:%s", qnode, myPred.get());
 		//myNode.set(myPred.get());
 		myNode.remove(); //解除myNode绑定在当前线程上，即使下次当前线程再次需要获得锁时则再创建一个QNode。
+		myPred.remove();
 	}
 	
-	private class QNode{
+	private static class QNode{
+		final int id;
+		
 		volatile boolean locked;
 		
 		final String name = Thread.currentThread().getName() + "-QNode";
 		
-		QNode(){}
+		private final static AtomicInteger idCount = new AtomicInteger(0);
+		
+		QNode(){
+			this.id = idCount.getAndIncrement();
+		}
 		
 		QNode(boolean locked) {
+			this();
 			this.locked = locked;
 		}
 
 		@Override
 		public String toString() {
-			return "QNode{locked:" + locked + ", name:" + name + "}";
+			return "QNode{id:" + id + ", locked:" + locked + ", name:" + name + "}";
 		}
 		
 	}
 	
 	//test below=================================
 	public static void main(String[] args) {
-		System.setProperty("lam.log.isAsyn", Boolean.FALSE.toString());
+		System.setProperty("lam.log.isAsyn", Boolean.TRUE.toString());
 		ThreadPoolExecutor executor = new ThreadPoolExecutor(
-				2, 
-				4, 
+				20, 
+				40, 
 				0, 
 				TimeUnit.MILLISECONDS, 
 				new LinkedBlockingQueue<Runnable>(),
