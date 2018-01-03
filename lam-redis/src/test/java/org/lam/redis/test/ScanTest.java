3adbbc743d8ba441d9e0f17c59a8321c152777fd
jjavaboy <jjavaboy@163.com>
2018-01-03 11:26:03 +0800

    redis scan

--- lam-redis/src/test/java/org/lam/redis/test/ScanTest.java
+++ lam-redis/src/test/java/org/lam/redis/test/ScanTest.java
@@ -30,9 +30,9 @@ public class ScanTest {
 		//jedis.scan(0); //scan(int cursor) has been deprecated, use scan(String cursor) instead.
 		String endCursor = "0";
 		String cursor = endCursor;
-		ScanParams scanParams = new ScanParams().count(5);
+		ScanParams scanParams = new ScanParams();
 		do {
-			ScanResult<String> scanResult = jedis.scan(cursor, scanParams);
+			ScanResult<String> scanResult = jedis.scan(cursor, scanParams.count(10));
 			cursor = scanResult.getStringCursor();
 			List<String> list = scanResult.getResult();
 			System.out.println("scan cursor:" + cursor);
@@ -43,6 +43,10 @@ public class ScanTest {
 		for (int j = 0; j < 100; j++) {
 			jedis.hset("hashkey", "field" + j, "value" + j);
 		}
+		/* 注意：
+		 * 在迭代一个编码为整数集合（intset，一个只由整数值构成的小集合）、 或者编码为压缩列表（ziplist，由不同值构成的一个小哈希或者一个小有序集合）时，
+		 * 增量式迭代命令通常会无视 COUNT 选项指定的值， 在第一次迭代就将数据集包含的所有元素都返回给用户。
+		 */
 		do {
 			ScanResult<Map.Entry<String, String>> sResult = jedis.hscan("hashkey", cursor, scanParams.match("field1*").count(5));
 			cursor = sResult.getStringCursor();
